5ef211c0f49e9e97bfe76dbb1eea02a0723ab7b0
VedantChavan03 <vedantchc@gmail.com>
2024-01-15 16:17:42 +0530

    Folder Structure

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/pages/Homepage.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false, home: homePage());
   }
 }
