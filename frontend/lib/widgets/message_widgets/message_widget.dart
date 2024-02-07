72b1570f20f031a41af8630027a02a9514940372
SuyeshJadhav <suyesh.j@somaiya.edu>
2024-02-07 13:35:42 +0530

    changed message border radius

--- frontend/lib/widgets/message_widgets/message_widget.dart
+++ frontend/lib/widgets/message_widgets/message_widget.dart
@@ -31,8 +31,15 @@ class MessageWidget extends StatelessWidget {
           Container(
             decoration: BoxDecoration(
               color: backgroundColor,
-              border: Border.all(color: Colors.black, width: 1),
-              borderRadius: BorderRadius.circular(5),
+              borderRadius: BorderRadius.circular(10),
+              // boxShadow: [
+              //   BoxShadow(
+              //     color: Colors.grey.withOpacity(0.4),
+              //     spreadRadius: 2,
+              //     blurRadius: 5,
+              //     offset: const Offset(0, 2),
+              //   ),
+              // ],
             ),
             child: InkWell(
               onTap: () {
