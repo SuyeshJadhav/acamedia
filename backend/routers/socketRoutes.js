720d12c004cb9a84d4e66bba6ea9705bed9f8691
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-03-22 12:53:31 +0530

    Private Messaging done

--- backend/routers/socketRoutes.js
+++ backend/routers/socketRoutes.js
@@ -1,9 +1,35 @@
 const { Router } = require("express");
 const router = Router();
 const path = require("path");
+
+const {
+  getUserData,
+  getDataById,
+} = require("../controllers/userControllers/userHelperFunctions");
+const { getChatId } = require("../chatManagement/chatHelperFunction");
+const initiateServer = require("../chatManagement/main");
+
 router.get("/", (req, res) => {
   const indexPath = path.join(__dirname, "..", "views", "index.html");
   res.sendFile(indexPath);
+});
+
+router.post("/direct", async (req, res) => {
+  const { sender_id, receiver_id } = req.body;
+  // console.log(sender_id, receiver_id);
+  const resp = await getDataById(receiver_id);
+  // console.log(resp);
+  const chatId = await getChatId(sender_id, receiver_id);
+  return res.status(201).json({ message: "Received ID: " + chatId });
+});
+
+router.get("/direct/:id", async (req, res) => {
+  const user1ID = "AXVw4MhGrTPqeAd0vuEG";
+  const user2ID = req.params.id;
+  console.log(user2ID);
+  const chatId = await getChatId(user1ID, user2ID);
+  console.log(chatId);
+  res.send("hello");
 });
 
 module.exports = router;
