49c12ed5568ab6b7826e7964366353fa4ebaefbf
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-03-31 14:31:14 +0530

    Frontend Test updated

--- backend/chatManagement/main.js
+++ backend/chatManagement/main.js
@@ -1,36 +1,53 @@
 const initiateServer = (io) => {
   io.use((socket, next) => {
-    const username = socket.handshake.auth.username;
-    if (!username) {
-      return next(new Error("invalid username"));
-    }
-    console.log(username);
-    socket.username = username;
+    const id = socket.handshake.auth.id;
+    socket.id = id;
     next();
   });
 
   io.on("connection", (socket) => {
-    socket.broadcast.emit("user connected", {
-      userID: socket.id,
-      username: socket.username,
-    });
-    const users = [];
-    for (let [id, socket] of io.of("/").sockets) {
-      users.push({
-        userID: id,
-        username: socket.username,
-      });
-    }
-    socket.emit("users", users);
+    // console.log(socket.rooms);
+    // console.log(socket.id);
+    // console.log("----------------");
 
-    socket.on("private message", ({ content, to }) => {
-      console.log(to, content);
-      socket.to(to).emit("private message", {
-        content,
-        from: socket.id,
+    socket.on("join", (roomName) => {
+      console.log(roomName);
+
+      Array.from(socket.rooms)
+        .filter((it) => it !== socket.id)
+        .forEach((id) => {
+          socket.leave(id);
+          socket.removeAllListeners(`emitMessage`);
+        });
+
+      socket.join(roomName);
+      // console.log(Array.from(socket.rooms));
+
+      socket.on(`emitMessage`, (message) => {
+        Array.from(socket.rooms)
+          .filter((it) => it !== socket.id)
+          .forEach((id) => {
+            socket.to(id).emit("onMessage", message);
+          });
       });
     });
-    // ...
+
+    socket.on("onMessage", (msg) => {
+      console.log(msg);
+    });
+
+    // socket.on("private message", ({ content, to }) => {
+    //   console.log(to, content);
+    //   socket.to(to).emit("private message", {
+    //     content,
+    //     from: socket.id,
+    //   });
+    // });
+
+    socket.on("disconnect", () => {
+      // console.log(socket.id + " ==== diconnected");
+      socket.removeAllListeners();
+    });
   });
 };
 
