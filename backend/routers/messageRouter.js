9bb3519ba8e7c1c224be751c8e81b99e03c9c74c
Aditya Chandran <adityachandrn1@gmail.com>
2024-04-02 00:57:47 +0530

    Fix: Message validation using Gemini API fixed, message controller optimized

--- backend/routers/messageRouter.js
+++ backend/routers/messageRouter.js
@@ -1,10 +1,23 @@
 const { Router } = require("express");
 const checkMessageValidity = require( "../controllers/messageController/messageValidity" );
+const { deleteMessage, storeMessage } = require( "../controllers/messageController/messageManagement" );
 const router = Router();
 
+router.post("/send", async (req, res) => {
+  const { senderId, senderRole, receiverRole, chatId, message } = req.body;
+  const result = await storeMessage(senderId, senderRole, receiverRole, chatId, message);
+  res.send(result);
+});
+
+router.delete("/delete", async (req, res) => {
+  const { senderId, chatId, messageId } = req.body;
+  const result = await deleteMessage(senderId, chatId, messageId);
+  res.send(result);
+});
+
 router.post("/prompt", async(req,res) => {
-  const message = req.body.message;
-  const result = await checkMessageValidity(message);
+  const {message, role} = req.body;
+  const result = await checkMessageValidity(message, role);
   res.send(result);
 });
 
--- backend/utils/variableNames.js
+++ backend/utils/variableNames.js
@@ -6,12 +6,18 @@ const collectionNames = {
 
 const statusCodes = {
   SERVER_ERROR: "ServerError",
+  //user
   USER_FOUND: "UserFound",
   USER_NOT_FOUND: "UserNotFound",
+  //chat
   CHAT_FOUND: "ChatFound",
   CHAT_NOT_FOUND: "ChatNotFound",
+  CHAT_NOT_IN_USER: "ChatNotInUser",
+  // message
   MESSAGE_SEND_SUCCESS: "MessageSendSuccess",
-  MESSAGE_SEND_FAIL: "MessageSendFail"
+  MESSAGE_SEND_FAIL: "MessageSendFail",
+  APPROPRIATE_MESSAGE: "AppropriateMessage",
+  INAPPROPRIATE_MESSAGE: "InappropriateMessage"
 };
 
 module.exports = { collectionNames, statusCodes };
